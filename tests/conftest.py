fde1de0aea031675dd16ad2c1456e79538e42301
Bryce <swarvingprogrammer@gmail.com>
2023-10-14 21:17:45 -0400

    Pytests for LVA2 API.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+"""LVA2 API Tests."""
 # Third-Party Libraries
 from django.core.management import call_command
 import pytest
@@ -10,14 +11,17 @@ from api.models import Address, Member, Rank
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
+    """Set up the django temp database."""
     with django_db_blocker.unblock():
+        call_command("loaddata", "db/committee.json")
+        call_command("loaddata", "db/hour_types.json")
         call_command("loaddata", "db/ranks.json")
         call_command("loaddata", "tests/test_db.json")
 
 
 @pytest.fixture
 def address_object_apt():
-    """Returns an Address Object"""
+    """Return an Address Object."""
     return Address(
         street_num=1482, street_name="Main St", box_area="28-05", apt_num="A"
     )
@@ -25,7 +29,7 @@ def address_object_apt():
 
 @pytest.fixture
 def address_object():
-    """Returns an Address Object"""
+    """Return an Address Object."""
     return Address(street_num=1482, street_name="Annapolis Rd", box_area="28-01")
 
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env pytest -vs
-"""Tests for Models."""
+"""Tests for Models for LVA2 API."""
 
 # Standard Python Libraries
 # Standard Libraries
 from datetime import datetime
 
 # Third-Party Libraries
-from django.core.exceptions import ValidationError
 import pytest
+from rest_framework.serializers import ValidationError
 
 # Custom Libraries
-from api.models import Address, Member
+from api.models import Address, CollateralDuty, Member, SleepIn, StandBy
 
 pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.django_db
-class TestMember:
-    """Test Member model."""
+class TestAddress:
+    """Test Address model."""
 
     pytestmark = pytest.mark.django_db
 
@@ -45,6 +45,137 @@ class TestMember:
             == "1482 Main St, A"
         )
 
+
+@pytest.mark.django_db
+class TestCollateralDuty:
+    """Test Collateral Duty model."""
+
+    pytestmark = pytest.mark.django_db
+
+    def test_str(self):
+        """Test proper str output."""
+        assert (
+            str(CollateralDuty.objects.filter(badge_num=12345).first())
+            == "Collateral Duty: Doe, John 2023-10-02 10:00 - 2023-10-02 13:59"
+        )
+
+    @pytest.mark.parametrize(
+        "end_time",
+        [
+            ("2023-10-02 13:30:00+00:00"),
+            ("2023-10-02 10:30:00+00:00"),
+            ("2023-10-02 11:00:00+00:00"),
+        ],
+    )
+    def test_losap_false(self, end_time):
+        """Test if losap_valid is false when less than min_hours."""
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        collateralDuty_obj.save()
+        assert collateralDuty_obj.losap_valid is False
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 06:00:00+00:00", "2023-10-01 09:59:00+00:00"),
+            ("2023-10-01 14:00:00+00:00", "2023-10-01 18:00:00+00:00"),
+            ("2023-10-02 15:00:00+00:00", "2023-10-02 18:59:00+00:00"),
+        ],
+    )
+    def test_clean_standBy_pass(self, start_time, end_time):
+        """Test Clean method focused on Stand By"""
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+        standBy_obj.start_time = datetime.fromisoformat(start_time)
+        standBy_obj.end_time = datetime.fromisoformat(end_time)
+        standBy_obj.save()
+
+        collateralDuty_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-02 07:00:00+00:00", "2023-10-02 10:59:00+00:00"),
+            ("2023-10-02 11:00:00+00:00", "2023-10-02 14:59:00+00:00"),
+            ("2023-10-02 10:00:00+00:00", "2023-10-02 13:59:00+00:00"),
+            ("2023-10-02 10:00:00+00:00", "2023-10-02 15:59:00+00:00"),
+            ("2023-10-02 09:00:00+00:00", "2023-10-02 13:59:00+00:00"),
+            ("2023-09-30 15:00:00+00:00", "2023-10-05 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_standBy_fail(self, start_time, end_time):
+        """Test Clean method focused on Stand By"""
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+        standBy_obj.start_time = datetime.fromisoformat(start_time)
+        standBy_obj.end_time = datetime.fromisoformat(end_time)
+        standBy_obj.save()
+        with pytest.raises(ValidationError):
+            collateralDuty_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-02 15:00:00+00:00", "2023-10-02 18:59:00+00:00"),
+            ("2023-10-03 06:00:00+00:00", "2023-10-03 09:59:00+00:00"),
+            ("2023-10-03 15:00:00+00:00", "2023-10-03 18:59:00+00:00"),
+        ],
+    )
+    def test_clean_sleepIn_pass(self, start_time, end_time):
+        """Test Clean method focused on Sleep In"""
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        collateralDuty_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 19:00:00+00:00", "2023-10-01 23:00:00+00:00"),
+            ("2023-10-02 05:00:00+00:00", "2023-10-02 09:00:00+00:00"),
+            ("2023-10-01 16:00:00+00:00", "2023-10-01 20:00:00+00:00"),
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 19:00:00+00:00"),
+            ("2023-09-30 15:00:00+00:00", "2023-10-05 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_sleepIn_fail(self, start_time, end_time):
+        """Test Clean method focused on Sleep In"""
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        with pytest.raises(ValidationError):
+            collateralDuty_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-02 07:00:00+00:00", "2023-10-02 10:59:00+00:00"),
+            ("2023-10-02 11:00:00+00:00", "2023-10-02 14:59:00+00:00"),
+            ("2023-10-02 09:00:00+00:00", "2023-10-02 18:59:00+00:00"),
+            ("2023-10-01 15:00:00+00:00", "2023-10-05 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_collateralDuty_obj_fail(self, start_time, end_time):
+        """Test Clean method focused on Collateral Duty."""
+        collateralDuty_object = CollateralDuty(
+            badge_num=Member(badge_num=12345, first_name="John", last_name="Doe"),
+            start_time=start_time,
+            end_time=end_time,
+        )
+        collateralDuty_object.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_object.end_time = datetime.fromisoformat(end_time)
+        with pytest.raises(ValidationError):
+            collateralDuty_object.clean()
+
+
+@pytest.mark.django_db
+class TestMember:
+    """Test Member model."""
+
+    pytestmark = pytest.mark.django_db
+
     def test_member_by_badge(self):
         mbr = Member.objects.filter(badge_num=12345)[0]
         assert mbr.badge_num == 12345
@@ -56,3 +187,203 @@ class TestMember:
     def test_member_full_member(self, member_object):
         """Test proper string returned by full member method."""
         assert member_object.full_member() == "Doe, John(12345)"
+
+
+@pytest.mark.django_db
+class TestSleepIn:
+    """Test Sleep In model."""
+
+    pytestmark = pytest.mark.django_db
+
+    def test_str(self):
+        """Test proper str output."""
+        assert (
+            str(SleepIn.objects.filter(badge_num=12345).first())
+            == "Sleep In: Doe, John 2023-10-01"
+        )
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 18:59:00+00:00"),
+            ("2023-10-02 07:00:00+00:00", "2023-10-02 11:00:00+00:00"),
+            ("2023-10-03 19:00:00+00:00", "2023-10-03 23:00:00+00:00"),
+        ],
+    )
+    def test_clean_collateralDuty_pass(self, start_time, end_time):
+        """Test Clean method focused on Collateral Duty"""
+        sleepIn_object = SleepIn.objects.filter(badge_num=12345).first()
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        collateralDuty_obj.save()
+        sleepIn_object.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 19:00:00+00:00", "2023-10-01 23:00:00+00:00"),
+            ("2023-10-02 05:00:00+00:00", "2023-10-02 09:00:00+00:00"),
+            ("2023-10-01 16:00:00+00:00", "2023-10-01 20:00:00+00:00"),
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 19:00:00+00:00"),
+            ("2023-09-30 15:00:00+00:00", "2023-10-03 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_collateralDuty_fail(self, start_time, end_time):
+        """Test Clean method focused on Collateral Duty_"""
+        sleepIn_object = SleepIn.objects.filter(badge_num=12345).first()
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        collateralDuty_obj.save()
+        with pytest.raises(ValidationError):
+            sleepIn_object.badge_num = Member.objects.get(badge_num=12345)
+            sleepIn_object.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 18:59:00+00:00"),
+            ("2023-10-02 07:00:00+00:00", "2023-10-02 11:00:00+00:00"),
+            ("2023-10-03 19:00:00+00:00", "2023-10-03 23:00:00+00:00"),
+        ],
+    )
+    def test_clean_standBy_pass(self, start_time, end_time):
+        """Test Clean method focused on Stand By"""
+        sleepIn_object = SleepIn.objects.filter(badge_num=12345).first()
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+        standBy_obj.start_time = datetime.fromisoformat(start_time)
+        standBy_obj.end_time = datetime.fromisoformat(end_time)
+        standBy_obj.save()
+        sleepIn_object.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 19:00:00+00:00", "2023-10-01 23:00:00+00:00"),
+            ("2023-10-02 05:00:00+00:00", "2023-10-02 09:00:00+00:00"),
+            ("2023-10-01 16:00:00+00:00", "2023-10-01 20:00:00+00:00"),
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 19:00:00+00:00"),
+            ("2021-05-30 15:00:00+00:00", "2023-10-03 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_standBy_fail(self, start_time, end_time):
+        """Test Clean method focused on Stand By"""
+        sleepIn_object = SleepIn.objects.filter(badge_num=12345).first()
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+        standBy_obj.start_time = datetime.fromisoformat(start_time)
+        standBy_obj.end_time = datetime.fromisoformat(end_time)
+        standBy_obj.save()
+        with pytest.raises(ValidationError):
+            sleepIn_object.badge_num = Member.objects.get(badge_num=12345)
+            sleepIn_object.clean()
+
+
+@pytest.mark.django_db
+class TestStandBy:
+    """Test Stand By model."""
+
+    pytestmark = pytest.mark.django_db
+
+    def test_str(self):
+        """Test proper str output."""
+        assert (
+            str(StandBy.objects.filter(badge_num=12345).first())
+            == "Stand By: Doe, John 2023-10-03 10:00 - 2023-10-03 13:59"
+        )
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 06:00:00+00:00", "2023-10-01 09:59:00+00:00"),
+            ("2023-10-01 14:00:00+00:00", "2023-10-01 18:00:00+00:00"),
+            ("2023-10-02 15:00:00+00:00", "2023-10-02 18:59:00+00:00"),
+        ],
+    )
+    def test_clean_collateralDuty_pass(self, start_time, end_time):
+        """Test Clean method focused on Collateral Duty"""
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        collateralDuty_obj.save()
+
+        standBy_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-03 07:00:00+00:00", "2023-10-03 10:59:00+00:00"),
+            ("2023-10-03 11:00:00+00:00", "2023-10-03 14:59:00+00:00"),
+            ("2023-10-03 10:00:00+00:00", "2023-10-03 13:59:00+00:00"),
+            ("2023-10-03 10:00:00+00:00", "2023-10-03 15:59:00+00:00"),
+            ("2023-10-03 09:00:00+00:00", "2023-10-03 13:59:00+00:00"),
+            ("2023-10-02 15:00:00+00:00", "2023-10-05 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_collateralDuty_fail(self, start_time, end_time):
+        """Test Clean method focused on Sleep In"""
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+
+        collateralDuty_obj = CollateralDuty.objects.filter(badge_num=12345).first()
+        collateralDuty_obj.start_time = datetime.fromisoformat(start_time)
+        collateralDuty_obj.end_time = datetime.fromisoformat(end_time)
+        collateralDuty_obj.save()
+        with pytest.raises(ValidationError):
+            standBy_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 18:59:00+00:00"),
+            ("2023-10-03 07:00:00+00:00", "2023-10-03 11:00:00+00:00"),
+            ("2023-10-03 15:00:00+00:00", "2023-10-03 18:59:00+00:00"),
+        ],
+    )
+    def test_clean_sleepIn_pass(self, start_time, end_time):
+        """Test Clean method focused on Sleep In"""
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+        standBy_obj.start_time = datetime.fromisoformat(start_time)
+        standBy_obj.end_time = datetime.fromisoformat(end_time)
+        standBy_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-01 19:00:00+00:00", "2023-10-01 23:00:00+00:00"),
+            ("2023-10-02 05:00:00+00:00", "2023-10-02 09:00:00+00:00"),
+            ("2023-10-01 16:00:00+00:00", "2023-10-01 20:00:00+00:00"),
+            ("2023-10-01 15:00:00+00:00", "2023-10-01 19:00:00+00:00"),
+            ("2023-09-30 15:00:00+00:00", "2023-10-05 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_sleepIn_fail(self, start_time, end_time):
+        """Test Clean method focused on Sleep In"""
+        standBy_obj = StandBy.objects.filter(badge_num=12345).first()
+        standBy_obj.start_time = datetime.fromisoformat(start_time)
+        standBy_obj.end_time = datetime.fromisoformat(end_time)
+        with pytest.raises(ValidationError):
+            standBy_obj.clean()
+
+    @pytest.mark.parametrize(
+        "start_time,end_time",
+        [
+            ("2023-10-03 07:00:00+00:00", "2023-10-03 10:59:00+00:00"),
+            ("2023-10-03 11:00:00+00:00", "2023-10-03 14:59:00+00:00"),
+            ("2023-10-03 09:00:00+00:00", "2023-10-03 18:59:00+00:00"),
+            ("2023-10-02 15:00:00+00:00", "2023-10-06 08:59:00+00:00"),
+        ],
+    )
+    def test_clean_standBy_fail(self, start_time, end_time):
+        """Test Clean method focused on Stand By."""
+        standBy_object = StandBy(
+            badge_num=Member(badge_num=12345, first_name="John", last_name="Doe"),
+            start_time="2023-10-01T10:00:00-00:00",
+            end_time="2023-10-01T14:00:00-00:00",
+        )
+        standBy_object.start_time = datetime.fromisoformat(start_time)
+        standBy_object.end_time = datetime.fromisoformat(end_time)
+
+        with pytest.raises(ValidationError):
+            standBy_object.clean()
