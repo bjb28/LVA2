d767d978dc31333c54239c94bc5ebf257a006c7b
Bryce <swarvingprogrammer@gmail.com>
2024-01-16 20:10:40 -0500

    Builds initial dev-container build.

---
+++ lva2/settings/production.py
@@ -0,0 +1,22 @@
+from lva2.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Need to explicitly enable logging for production configurations
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
